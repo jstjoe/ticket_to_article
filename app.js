6ea64b506f4364254845be8c3a6b1551c4984120
Joseph <joe@josephmccarron.com>
2014-02-15 23:08:44 -0800

    get comments

--- app.js
+++ app.js
@@ -2,11 +2,28 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'init',
+      'getComments.done':'renderComments'
     },
-
-    doSomething: function() {
+    requests: {
+      getComments: function() {
+        return {
+          url: helpers.fmt('/api/v2/tickets/%@/comments.json',this.ticket().id()),
+          dataType: 'JSON',
+          type: 'GET',
+          proxy_v2: true
+        };
+      },
+    },
+    init: function() {
+      this.getComments();
+    },
+    getComments: function(id) {
+      this.ajax('getComments', id);
+    },
+    renderComments: function(data) {
+      var comments = data.comments;
+      console.log(comments);
     }
   };
-
 }());
